bfcb5ee509340ce58c0ce278c5d591814bede4c6
Bala <Bala@Balas-MacBook-Pro.local>
2026-05-06 10:28:31 +0200

    secondphas1

--- todo.c
+++ todo.c
@@ -4,10 +4,18 @@ static void	display_list(t_todo *todo)
 {
 	t_task	*current;
 
+	if (!todo->head)
+	{	
+		printf("\nNo Pending Tasks: Phew!\n");
+		return ;
+	}
 	current = todo->head;
 	while (current)
 	{
-		printf("\n%s - Urgency : %d\n", current->title, current->priority);
+		if (current->done == 1)
+			printf("✅ %s \n", current->title);
+		else
+			printf("• %s - Urgency : %d\n", current->title, current->priority);
 		current = current->next;
 	}
 }
@@ -38,11 +46,17 @@ void	add_task(t_todo *todo)
 		return ;
 	printf("\nEnter a task: ");
 	fgets(temp_title, 100, stdin);
-	printf("\nEnter task priority(1 to 5): ");
+	printf("\nEnter task priority(1 to 4): ");
 	fgets(temp_priority, 3, stdin);
+	new_task->priority = atoi(temp_priority);
+	while (!(new_task->priority >= 1 && new_task->priority <= 4))
+	{
+		printf("\nEnter a valid response (1 to 4) : ");
+		fgets(temp_priority, 3, stdin);
+		new_task->priority = atoi(temp_priority);
+	}
 	strprune(temp_title);
 	strcpy(new_task->title, temp_title);
-	new_task->priority = atoi(temp_priority);
 	new_task->done = 0;
 	new_task->id = todo->count + 1; // adding id
 	new_task->next = todo->head; // coping todo->head null to new next
@@ -53,10 +67,48 @@ void	add_task(t_todo *todo)
 
 void	del_task(t_todo *todo)
 {
+	t_task	*current;
+	t_task	*previous;
+	char	temp_del[100];
+	int		del_id;
 
+	current = todo->head;
+	previous = NULL;
+
+	printf("\nEnter ID to Delete Task");
+	fgets(temp_del,100,stdin);
+	del_id = atoi(temp_del);
+	while (current)
+	{
+		if (current->id == del_id)
+		{
+			previous = current;
+			current->next->next = current->next;
+		}
+
+	}
 }
 
 void	mark_done(t_todo *todo)
 {
+	t_task	*current;
+	char	temp_mark_id[100];
+	int		mark_id;
 
+	printf("\nEnter ID to Mark Done : ");
+	fgets(temp_mark_id,100,stdin);
+	mark_id = atoi(temp_mark_id);
+	current = todo->head;
+	while(current)
+	{
+		if(current->id == mark_id)
+		{
+			current->done = 1;
+			display_list(todo);
+			return ;
+		}
+		current = current->next;
+	}
+	printf("Incorrect ID ! \n");
+	display_list(todo);
 }
